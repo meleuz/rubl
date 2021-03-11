a3b3360ce0056910c4525004c51999e3270f7069
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-11 21:38:50 +0500

    Main.java ADDED

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        double currentBalance = 100.55;
+        double payment = 1500.74;
         int currentBalance = 100_55;
         int payment = 1517_74;
 
+        double bonus;
         int bonus;
         if (payment >= 1000) {
+            bonus = payment / 100;
             bonus = payment / 100 / 100;
         } else {
             bonus = 0;
         }
-
-        System.out.println(bonus);
-    }
-}
